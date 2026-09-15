386e9dd7841e2972c20d021a91fbc203825395bf
Sk Hajrat <hajratsk168@gmail.com>
2026-09-15 19:46:42 +0530

    Bug Fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 let cached=global.mongoose || {conn:null,promise:null};
+global.mongoose = cached;//added
 
 export default async function connectDB() {
     if(cached.conn) return cached.conn;
@@ -12,7 +13,7 @@ export default async function connectDB() {
             cached.conn= await cached.promise;
         }catch(error){
             console.error("Error to connect MongoDB : ",error);
-            
+            cached.promise = null;//added
         }
     }
     return cached.conn;
--- models/User.js
+++ models/User.js
@@ -1,18 +1,37 @@
+// import mongoose from "mongoose";
+
+
+
+// const UserSchema = new mongoose.Schema(
+//     {
+//         _id:{type:String,required:true},
+//         name:{type:String,required:true},
+//         email:{type:String,required:true},
+//         image:{type:String,required:false}
+//     },
+//     {timestamps:true}
+// );
+
+// const User = mongoose.models.User || mongoose.model("User",UserSchema)
+
+// export default User;
+
 import mongoose from "mongoose";
 
-
-
 const UserSchema = new mongoose.Schema(
-    {
-        _id:{type:String,required:true},
-        name:{type:String,required:true},
-        email:{type:String,required:true},
-        image:{type:String,required:false}
+  {
+    _id: { type: String, required: true }, // Clerk ID
+    name: { type: String, required: true },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true, 
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"] 
     },
-    {timestamps:true}
+    image: { type: String }
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.models.User || mongoose.model("User",UserSchema)
-
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 export default User;
-
