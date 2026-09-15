e0ea66073d9dad791f3720234279e6faf2594700
Sk Hajrat <hajratsk168@gmail.com>
2026-09-15 12:39:00 +0530

    Add code.

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        _id:{type:String,required:true},
+        _name:{type:String,required:true},
+        _email:{type:String,required:true},
+        _image:{type:String,required:false}
+    },
+    {timestamps:true}
+);
+
+const User = mongoose.models.User || mongoose.model("User",UserSchema)
+
+export default User;
